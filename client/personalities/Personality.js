cee03f96c1e3e2da3e4f6c4c12c878ee87856d73
Richard Sipes <rksipes@gmail.com>
2017-02-21 01:53:52 -0800

    update comment

--- client/personalities/Personality.js
+++ client/personalities/Personality.js
@@ -1,19 +1,3 @@
-// Template.Personality.onCreated(function() {
-//     // Set a variable scoped to this template
-//     this.editMode = new ReactiveVar(false);
-// });
-
-// Template.Personality.helpers({
-//     // returns the current recipe's id
-//     updateRecipeId: function() {
-//         return this._id;
-//     },
-//     editMode: function() {
-//         // return the variable scoped to this template
-//         return Template.instance().editMode.get();
-//     }
-// })
-
 Template.Personality.events({
     'click .fa-trash': function() {
         Meteor.call('deletePersonality', this._id);
